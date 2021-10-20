b741c4d5447af41b879a20b90e2920ac5f8450ed
Tharindu7Kanchana <48875349+Tharindu7Kanchana@users.noreply.github.com>
2021-10-21 01:21:25 +0530

    heroku authetication push

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  PORT: 5432 || process.env.PGPORT,
   HOST: "localhost" || process.env.PGHOST,
   USER: "postgres" || process.env.PGUSER,
   PASSWORD: "testdb" || process.env.PGPASSWORD,
   DB: "testdb" || process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   dialect: "postgres",
   pool: {
     max: 5,
--- models/index.js
+++ models/index.js
@@ -2,17 +2,8 @@ const config = require("../config/db.config.js");
 const { DataTypes } = require("sequelize");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-  host: config.HOST,
+const sequelize = new Sequelize(config.connectionString, {
   dialect: config.dialect,
-  operatorsAliases: false,
-
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle,
-  },
 });
 
 const db = {};
