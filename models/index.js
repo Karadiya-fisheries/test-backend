e612132d0da85f7026830c575e845bdccb1d6f9f
Tharindu7Kanchana <48875349+Tharindu7Kanchana@users.noreply.github.com>
2021-10-21 10:10:04 +0530

    ss

--- models/index.js
+++ models/index.js
@@ -17,7 +17,7 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   ssl: true,
-  dialect: env.dialect,
+  dialect: config.dialect,
   dialectOptions: {
     ssl: true,
   },
