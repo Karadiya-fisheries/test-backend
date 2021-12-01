c4e63829ac0d8489ade171d7677fbdde23f96296
Tharindu7Kanchana <48875349+Tharindu7Kanchana@users.noreply.github.com>
2021-12-01 12:04:25 +0530

    heroku deploy

--- index.js
+++ index.js
@@ -23,26 +23,27 @@ app.use(flash());
 const db = require("./models");
 const Role = db.role;
 
-db.sequelize.sync({ force: true }).then(() => {
-  initial();
-});
+db.sequelize.sync();
+// ({ force: true }).then(() => {
+//     initial();
+//   });
 
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user",
-  });
+//   function initial() {
+//     Role.create({
+//       id: 1,
+//       name: "user",
+//     });
 
-  Role.create({
-    id: 2,
-    name: "moderator",
-  });
+//     Role.create({
+//       id: 2,
+//       name: "moderator",
+//     });
 
-  Role.create({
-    id: 3,
-    name: "admin",
-  });
-}
+//     Role.create({
+//       id: 3,
+//       name: "admin",
+//     });
+//   }
 
 app.use("/fishermen", fishermenRoute);
 app.use("/boats", boatRoute);
