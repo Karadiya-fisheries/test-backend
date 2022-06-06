87fce5281dd1b1068e7142ca2dc360e205cead76
Tharindu7Kanchana <7tharindugalle@gmail.com>
2022-06-07 00:55:44 +0530

    routing set

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const passport = require("passport");
 const session = require("express-session");
 const flash = require("express-flash");
 const cookieParser = require("cookie-parser");
-const fishermenRoute = require("./routes/fishermenRoute");
-const boatRoute = require("./routes/boatRoute");
+const fishermenRoute = require("./routes/fishermen.routes");
+const boatRoute = require("./routes/boat.routes");
 const catchRoute = require("./routes/catch.routes");
 const departureRoute = require("./routes/departure.routes");
+const triplogRoute = require("./routes/triplog.routes");
+const ownerRoute = require("./routes/owner.routes");
+const statRoute = require("./routes/stat.routes");
 const app = express();
 
 app.use(cors());
@@ -21,35 +24,37 @@ app.use(passport.session());
 app.use(flash());
 
 const db = require("./models");
+const { owner } = require("./models");
 const Role = db.role;
 
-db.sequelize.sync();
-// ({ force: true }).then(() => {
-//     initial();
-//   });
+db.sequelize.sync({ force: true }).then(() => {
+  initial();
+});
 
-//   function initial() {
-//     Role.create({
-//       id: 1,
-//       name: "user",
-//     });
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
 
-//     Role.create({
-//       id: 2,
-//       name: "moderator",
-//     });
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
 
-//     Role.create({
-//       id: 3,
-//       name: "admin",
-//     });
-//   }
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
+}
 
 app.use("/fishermen", fishermenRoute);
 app.use("/boats", boatRoute);
 app.use("/catches", catchRoute);
 app.use("/departure", departureRoute);
-
+app.use("/owner", ownerRoute);
+app.use("/triplog", triplogRoute);
+app.use("/stat", statRoute);
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
 
